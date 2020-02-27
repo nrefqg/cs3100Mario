5b6293281c4129eda91ab365d4b949665859fb41
Michael Pieper <michaelepieper@gmail.com>
2020-02-27 00:26:31 -0600

    Add basic framework for displaying pygame

--- base_game.py
+++ base_game.py
@@ -1,8 +1,26 @@
 import pygame
+from enemy import Enemy
+from hitbox import Hitbox
 from pygame.locals import *
 
 pygame.init()
 
-# Start writing game code here
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
+pygame.display.set_caption("CS 3100 Project")
+
+game_ended = False
+clock = pygame.time.Clock()
+
+while not game_ended:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_ended = True
+
+    clock.tick(60)
+    pygame.display.flip()
+
+pygame.quit()
 
