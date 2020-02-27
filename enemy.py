6c0c0c68be111b0289232e9ac5cc479714db61de
Michael Pieper <michaelepieper@gmail.com>
2020-02-27 01:19:38 -0600

    Enemy inherits from Sprite, takes in image and coordinate, places on screen

--- enemy.py
+++ enemy.py
@@ -1,12 +1,25 @@
-from hitbox import Hitbox
+import pygame
+import os
 
 
-class Enemy:
+class Enemy(pygame.sprite.Sprite):
 
-    def __init__(self, name, hitbox):
-        if type(name) is not str:
+    def __init__(self, image, x, y):
+        super().__init__()
+        if type(image) is not str:
             raise ValueError("Argument name should be a string")
-        if not isinstance(hitbox, Hitbox):
-            raise ValueError("Argument hitbox should be a Hitbox object")
-        self._name = name
-        self._hitbox = hitbox
+        if type(x) is not int:
+            raise ValueError("Argument x should be an int")
+        if type(y) is not int:
+            raise ValueError("Argument y should be an int")
+
+        self.image = image
+        self.x = x
+        self.y = y
+        self.position = 0
+
+        self.image = pygame.image.load(os.path.join('sprites', image))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
