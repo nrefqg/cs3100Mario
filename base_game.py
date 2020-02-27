d225f59e6b7e3bfe677168dcf2a77e6f42d81801
Michael Pieper <michaelepieper@gmail.com>
2020-02-27 01:20:53 -0600

    Update base game to display and move single enemy

--- base_game.py
+++ base_game.py
@@ -1,15 +1,19 @@
 import pygame
 from enemy import Enemy
-from hitbox import Hitbox
+from enemy1 import Enemy1
 from pygame.locals import *
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 750
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 pygame.display.set_caption("CS 3100 Project")
+
+enemy1 = Enemy1(300, 500)
+enemy_list = pygame.sprite.Group()
+enemy_list.add(enemy1)
 
 game_ended = False
 clock = pygame.time.Clock()
@@ -18,6 +22,12 @@ while not game_ended:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_ended = True
+
+    screen.fill((100, 177, 232))
+
+    enemy_list.draw(screen)
+    for enemy in enemy_list:
+        enemy.move(4)
 
     clock.tick(60)
     pygame.display.flip()
