05c77f1911a4179af3c66c151df6bb9520d58e06
Austin Poulsen <ajp8x9@umsystem.edu>
2020-03-15 20:59:27 -0500

    Character appears on screen, bugged still WIP

--- base_game.py
+++ base_game.py
@@ -13,15 +13,40 @@ clock = pygame.time.Clock()
 display = (500, 500)
 scale = pygame.Surface((300, 200))
 screen = pygame.display.set_mode(display, 0, 32)
-screen.fill((146, 244, 255))
 
+screen.fill((146, 244, 255))
 file_rendering.render(screen)
 
+player_image = pygame.image.load('character/mario.png')
+
+moving_right = False
+moving_left = False
+
+player_location = [50,50]
+
 while True:
+
+    screen.blit(player_image, player_location)
+
+    if moving_right == True:
+        player_location[0] += 4
+    if moving_left == True:
+        player_location[0] -= 4
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() #Stop pygame
             sys.exit() #Stop script
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_RIGHT:
+                moving_right = True
+            if event.key == K_LEFT:
+                moving_left = True
+        if event.type == pygame.KEYUP:
+            if event.key == K_RIGHT:
+                moving_right = False
+            if event.key == K_LEFT:
+                moving_left = False
 
     pygame.display.update()
     #Keeps game at 60fps
