c824b325a2b031f623f1a3655f675420710a4a26
Timm-Tomm <61246550+Timm-Tomm@users.noreply.github.com>
2020-04-04 16:08:38 -0500

    Update hiddenBlock.py

--- blocks/hiddenBlock.py
+++ blocks/hiddenBlock.py
@@ -3,4 +3,11 @@ from blocks.blocks import Block
 
 class hiddenBlock(Block):
 	def __init__(self, x, y):
-		super().__init__("hiddenBlock.png", x, y, [16, 16])
+		super().__init__("hiddenBlock.png", x, y)
+		self.xHitRight = x+32
+		self.xHitBottom = y+32
+
+	def disabled(self):
+		super().__init__("disabledBlock.png", x, y)
+		self.xHitRight = x+32
+		self.xHitBottom = y+32
