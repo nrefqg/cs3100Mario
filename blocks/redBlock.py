6ea060691e13be0be73a922c907cde7245cfcc0c
Timm-Tomm <61246550+Timm-Tomm@users.noreply.github.com>
2020-04-04 16:08:46 -0500

    Update redBlock.py

--- blocks/redBlock.py
+++ blocks/redBlock.py
@@ -1,6 +1,8 @@
-#import pygame
+import pygame
 from blocks.blocks import Block
 
-class redBlock(Block):
+class ground(Block):
 	def __init__(self, x, y):
-		super().__init__("redBlock.png", x, y, [16, 16])
+		super().__init__("ground.png", x, y)
+		self.xHitRight = x+32
+		self.xHitBottom = y+32
