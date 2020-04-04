fc2eecc6cac9302a8fa0aaaa7e455a50578518bf
Timm-Tomm <61246550+Timm-Tomm@users.noreply.github.com>
2020-04-04 16:08:40 -0500

    Update pipe.py

--- blocks/pipe.py
+++ blocks/pipe.py
@@ -4,13 +4,24 @@ from blocks.blocks import Block
 class leftPipe(Block):
 	def __init__(self, x, y):
 		super().__init__("leftPipe.png", x, y)
+		self.xHitRight = x+32
+		self.xHitBottom = y+32
 
 
 class rightPipe(Block):
 	def __init__(self, x, y):
 		super().__init__("rightPipe.png", x, y)
+		self.xHitRight = x+32
+		self.xHitBottom = y+32
 
 
-class midPipe(Block):
+class topPipe(Block):
 	def __init__(self, x, y):
-		super().__init__("midPipe.png", x, y)
+		super().__init__("topPipe.png", x, y)
+		self.xHitRight = x+32
+		self.xHitBottom = y+32
+
+class hiddenPipe(Block):
+	def __init__(self, x, y):
+		super().__init__("hiddenPipe.png", x, y)
+
