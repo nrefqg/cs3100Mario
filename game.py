b356b0630817122d8875cf678e5908367d1136d7
Timm-Tomm <61246550+Timm-Tomm@users.noreply.github.com>
2020-04-15 14:19:52 -0500

    Updating for new movement

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import file_rendering
 import file_loader
+import time
 from itertools import combinations
 from enemies.enemy0 import Enemy0
 from enemies.enemy1 import Enemy1
@@ -32,8 +33,10 @@ player_image = pygame.image.load('sprites/mario.png')
 player_location = [20,320]
 player_rect = pygame.Rect(100, 100, 5, 13)
 
+#default movement indicators
 moving_right = False
 moving_left = False
+jumping = False
 
 # Load in enemy list
 enemy_list = pygame.sprite.Group()
@@ -49,33 +52,90 @@ viewport = Viewport(SCREEN_WIDTH, SCREEN_HEIGHT)
 #A list of all rects in the level
 allRects = file_rendering.render(level)
 
+#initial momentum value
+player_x_momentum = 0
+player_y_momentum = 0
+
 while True:
     #Fills the background with a light blue color
     viewport.reset()
 
+    #if character falls below floor, resets to floor.  Will fix once collisions are implemented
+    if player_location[1] > 208:
+        player_location[1] = 208
+
+    #Sprinting and horizontal movement
+    if (moving_right or moving_left) and player_x_momentum < 2.0:
+        player_x_momentum += 0.1
+    elif player_x_momentum >= 0.1:
+        player_x_momentum -= 0.5
+    else:
+        player_x_momentum = 0
+
+    #default gravity if mario is in air and not jumping
+    if not jumping:
+        if player_location[1] < 208:
+            player_y_momentum += 0.3
+        else:
+            player_y_momentum = 0
+
+        if player_location[1] + player_y_momentum < 208:
+            player_location[1] += player_y_momentum
+        else:
+            player_location[1] = 208
+    else:   #handles mario jump momentum
+        if player_location[1] <= 208 and player_location[1] >= 191:
+            player_y_momentum += 1
+        elif player_location[1] < 191 and player_location[1] >= 174:
+            player_y_momentum += 0.5
+        elif player_location[1] < 174 and player_location[1] >= 157:
+            player_y_momentum -= 1
+        elif player_location[1] < 157 and player_location[1] >= 140:
+            player_y_momentum -= 0.25
+
+        #handles positioning
+        if player_location[1] > 140:
+            player_location[1] -= player_y_momentum
+        elif player_location[1] <= 147 and player_location[1] > 140:
+            player_location[1] -= 0.1
+        else:
+            player_location[1] = 140
+            player_y_momentum = 0
+            jumping = False
+
     #Movement for the player is modified when specific keypresses are made
     if moving_right == True:
-        player_location[0] += 4
-    if moving_left == True and player_location[0] > viewport.offsetX:
-        player_location[0] -= 4
+        player_image = pygame.image.load('sprites/mario.png')
+        player_location[0] += player_x_momentum
+
+    if moving_left == True:
+        player_image = pygame.image.load('sprites/marioflip.png')
+        player_location[0] -= player_x_momentum
 
     #Check pygame for "events" such as button presses
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.display.quit()
             pygame.quit()  # Stop pygame
             sys.exit()  # Stop script
 
         # Respond to player keys
         if event.type == pygame.KEYDOWN:
+
             if event.key == pygame.K_RIGHT:
                 moving_right = True
             if event.key == pygame.K_LEFT:
                 moving_left = True
+            if event.key == pygame.K_SPACE and player_location[1] == 208:
+                jumping = True
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 moving_right = False
             if event.key == pygame.K_LEFT:
                 moving_left = False
+            if event.key == pygame.K_SPACE:
+                jumping = False
+                player_y_momentum = 0
 
     # Update sprites on screen
     viewport.render_sprites(player_image, player_location, enemy_list, block_list, pipe_list)
